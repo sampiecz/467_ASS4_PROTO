10e3df931273e30617f0b686041ace47244e3475
sampiecz <samuel.piecz@gmail.com>
2018-10-21 22:39:57 -0500

    Showed Phil how to use CSS and the HTML class parameter.

--- create_a_new_band.php
+++ create_a_new_band.php
@@ -1,6 +1,6 @@
 <?php include 'header.html'; ?>
 
-<!-- Connect to the database -->
+<!-- Start: Connect to the database -->
 <?php
     try {
         $dsn = "mysql:host=courses;dbname=z1732715";
@@ -12,5 +12,46 @@
         echo "Connection to database failed: " . $e->getMessage();
     }
 ?>
+<!-- End: Connect to the database -->
+
+<style>
+
+    .blue-and-black
+    {
+        background: blue;
+        border: 1px solid black;
+        width: 50%;
+        float: left;
+    }
+    .red-and-black
+    {
+        background: red;
+        border: 1px solid black;
+        width: 50%;
+        float: right;
+    }
+
+    @media only screen and (max-width: 767px)
+    {
+        .red-and-black
+        {
+            width: 100%;
+            float: right;
+        }
+
+        .blue-and-black
+        {
+            float: none;
+            width: 100%;
+        }
+    }
+</style>
+
+<div class="blue-and-black">
+    <p>Example</p>
+</div>
+<div class="red-and-black">
+    <p>Example</p>
+</div>
 
 <?php include 'footer.html'; ?>
