8107aa705093000a3edda7de392778c70e13cd3e
sampiecz <samuel.piecz@gmail.com>
2018-10-23 13:32:13 -0500

    Made the home page even sexier.

--- index.php
+++ index.php
@@ -17,10 +17,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="jumbotron jumbotron-fluid">
+                    <div class="jumbotron jumbotron-fluid" style="padding: 0px;">
+                      <div class="jumbotron-image"></div>
                       <div class="container">
-                        <h1 class="display-4">Event management simplified</h1>
-                        <p class="lead">Empowering you to better manage your events and focus on what matters.</p>
+                        <div class="bg-text">
+                            <h1 class="display-4">Event management simplified</h1>
+                            <p class="lead" style="color: white;">Empowering you to better manage events and focus on what matters.</p>
+                        </div>
                       </div>
                     </div>
                 </div>
@@ -36,32 +39,35 @@
                       <div class="container">
 						<div class="row">
 							<div class="col-12">
-								<h2>Create</h2>
+								<h2>Create artist, bands, and events</h2>
 								<hr>
 							</div>
 							<div class="col-lg-4 col-md-4 col-sm-12">
 								<div class="card">
+                                  <img class="card-img-top" src="https://images.unsplash.com/photo-1527261834078-9b37d35a4a32?ixlib=rb-0.3.5&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;s=309fe659f0d0e2a5b7408b7a756613c2&amp;dpr=1&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80&amp;cs=tinysrgb"/>
 								  <div class="card-body">
-									<h5 class="card-title">Create artists</h5>
+									<h5 class="card-title">Create an artist</h5>
 									<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-									<a href="create_a_new_artist.php" class="btn btn-primary">Create artist</a>
+									<a href="create_a_new_artist.php" class="btn btn-primary">Create an artist</a>
 								  </div>
 								</div>    
 							</div>
 							<div class="col-lg-4 col-md-4 col-sm-12">
 								<div class="card">
+                                  <img class="card-img-top" src="https://images.unsplash.com/photo-1518552722618-737f6d8e91c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=20e3528b582270383b6b01e784e50c7c&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb"/>
 								  <div class="card-body">
-									<h5 class="card-title">Create bands</h5> <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-									<a href="create_a_new_band.php" class="btn btn-primary">Create band</a>
+									<h5 class="card-title">Create a band</h5> <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+									<a href="create_a_new_band.php" class="btn btn-primary">Create a band</a>
 								  </div>
 								</div>    
 							</div>
 							<div class="col-lg-4 col-md-4 col-sm-12">
 								<div class="card">
+                                  <img class="card-img-top" src="https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ae8584287da6fac5895bbe4fb80f9f5f&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb"/>
 								  <div class="card-body">
-									<h5 class="card-title">Create events</h5>
+									<h5 class="card-title">Create an event</h5>
 									<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-									<a href="create_a_new_event.php" class="btn btn-primary">Create event</a>
+									<a href="create_a_new_event.php" class="btn btn-primary">Create an event</a>
 								  </div>
 								</div>    
 							</div>
@@ -81,15 +87,15 @@
                       <div class="container">
 						<div class="row">
 							<div class="col-12">
-								<h2>Approve</h2>
+								<h2>Approve events</h2>
 								<hr>
 							</div>
 							<div class="col-12">
 								<div class="card">
 								  <div class="card-body">
-									<h5 class="card-title">Approve events</h5>
+									<h5 class="card-title">Approve an event</h5>
 									<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-									<a href="create_a_new_artist.php" class="btn btn-primary">Approve event</a>
+									<a href="create_a_new_artist.php" class="btn btn-primary">Approve an event</a>
 								  </div>
 								</div>    
 							</div>
