467be83c1317c893fb090ecc5e32f5fefb81d4ff
Learnallthetime <philipboffa@gmail.com>
2018-10-28 17:48:59 -0500

    syntax

--- create_a_new_artist.php
+++ create_a_new_artist.php
@@ -39,6 +39,35 @@
                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Casper">
                           </div>
                           <div class="form-group">
+                            <label for="exampleFormControlInput1">Enter artist last name</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Doe">
+                          </div>
+			  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    
+  </button>
+  			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Male</a>
+    <a class="dropdown-item" href="#">Female</a>
+    <a class="dropdown-item" href="#">Choose not to answer</a>
+  </div>
+ <div class="form-group">
+                            <label for="exampleFormControlInput1">Enter artist email address</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@hotmail.com">
+</div>
+
+ <div class="form-group">
+                            <label for="exampleFormControlInput1">Enter artist phone number</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="630 420 6666">
+</div>
+
+
+
+
+
+
+
+                          <div class="form-group">
                             <button type="submit" class="btn btn-success">Create artist</button>
                           </div>
                         </form>
--- generate_objs.sql
+++ generate_objs.sql
@@ -16,14 +16,14 @@ DROP TABLE IF EXISTS Event;
 CREATE TABLE Artist 
 (
     artistId integer AUTO_INCREMENT,
-    name Char(255),
+    name Char(255) NOT NULL, middleN Char(255) NOT NULL, lastN Char(255) NOT NULL, gender Char(20) NOT NULL, email Char(255) NOT NULL, number Char(25) NOT NULL
     primary key (artistId)
 );
 
 CREATE TABLE Band 
 (
     bandId integer AUTO_INCREMENT,
-    name Char(255),
+    name Char(255) NOT NULL, member Char(255) NOT NULL , rate Char(255)NOT NULL, blNumber Char(255) NOT NULL, aStreet Char(255) NOT NULL, aCity Char(255) NOT NULL, aState Char(255) NOT NULL, aZip Char(255) NOT NULL, aEmail Char(255) NOT NULL, aPhone Char(255) NOT NULL 
     primary key (bandId)
 );
 
