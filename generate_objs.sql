6ef7a14c74dfe8b496f46ef74517afd9deeb18dd
sampiecz <samuel.piecz@gmail.com>
2018-10-30 10:08:43 -0500

    Fixed indentation.

--- generate_objs.sql
+++ generate_objs.sql
@@ -16,14 +16,28 @@ DROP TABLE IF EXISTS Event;
 CREATE TABLE Artist 
 (
     artistId integer AUTO_INCREMENT,
-    name Char(255) NOT NULL, middle_Name Char(255) NOT NULL, last_Name Char(255) NOT NULL, gender Char(20) NOT NULL, email Char(255) NOT NULL, number Char(25) NOT NULL,
+    name Char(255) NOT NULL,
+    middle_Name Char(255) NOT NULL,
+    last_Name Char(255) NOT NULL,
+    gender Char(20) NOT NULL,
+    email Char(255) NOT NULL,
+    number Char(25) NOT NULL,
     primary key (artistId)
 );
 
 CREATE TABLE Band 
 (
     bandId integer AUTO_INCREMENT,
-    name Char(255) NOT NULL, member Char(255) NOT NULL , rate Char(255)NOT NULL, bl_Number Char(255) NOT NULL, ag_Street Char(255) NOT NULL, ag_City Char(255) NOT NULL, ag_State Char(255) NOT NULL, ag_Zip Char(255) NOT NULL, ag_Email Char(255) NOT NULL, ag_Phone Char(255) NOT NULL, 
+    name Char(255) NOT NULL,
+    member Char(255) NOT NULL,
+    rate Char(255)NOT NULL,
+    bl_Number Char(255) NOT NULL,
+    ag_Street Char(255) NOT NULL,
+    ag_City Char(255) NOT NULL,
+    ag_State Char(255) NOT NULL,
+    ag_Zip Char(255) NOT NULL,
+    ag_Email Char(255) NOT NULL,
+    ag_Phone Char(255) NOT NULL, 
     primary key (bandId)
 );
 
