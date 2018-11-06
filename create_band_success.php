db1c7a83d9a28e1141df0269b89190cfae3c96f6
Learnallthetime <philipboffa@gmail.com>
2018-11-06 16:06:36 -0600

    Php is working for band_Success. Sweet.

--- create_band_success.php
+++ create_band_success.php
@@ -1,17 +1,6 @@
 
 <?php include 'header.php'; ?>
 
-<?php
-            try {
-                $dsn = "mysql:host=courses;dbname=z1732715";
-                $username = "z1732715";
-                $password = "1996Apr23";
-                $pdo = new PDO($dsn,$username, $password);
-            }
-            catch(PDOexception $e) {
-                echo "Connection to database failed: " . $e->getMessage();
-            }
-        ?>
 
         
         <?php
