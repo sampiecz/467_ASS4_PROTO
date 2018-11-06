7160ee21bda9727cc40591284c2275a1670c3b65
sampiecz <samuel.piecz@gmail.com>
2018-11-05 23:32:59 -0600

    Removed all .save files.

--- create_a_new_artist.php
+++ create_a_new_artist.php
@@ -16,46 +16,56 @@
                 <div class="row">
                     <div class="col-12">
                         <form method="post" action="">
-                          <div class="form-group">
-                            <label for="exampleFormControlInput1">Enter artist first name</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John">
-                          </div>
-                          <div class="form-group">
-                            <label for="exampleFormControlInput1">Enter artist middle name</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Casper">
-                          </div>
-                          <div class="form-group">
-                            <label for="exampleFormControlInput1">Enter artist last name</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Doe">
-                          </div>
-			  <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    
-  </button>
-  			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Male</a>
-    <a class="dropdown-item" href="#">Female</a>
-    <a class="dropdown-item" href="#">Choose not to answer</a>
-  </div>
- <div class="form-group">
-                            <label for="exampleFormControlInput1">Enter artist email address</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@hotmail.com">
-</div>
-
- <div class="form-group">
-                            <label for="exampleFormControlInput1">Enter artist phone number</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="630 420 6666">
-</div>
-
- 	 
-
-
-
-
-
-                          <div class="form-group">
-                            <button type="submit" class="btn btn-success">Create artist</button>
-                          </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <h3>Artist Information</h3>
+                                        <label for="exampleFormControlInput1">First Name</label>
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="John">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlInput1">Middle Name</label>
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Casper">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlInput1">Last Name</label>
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Doe">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlInput1">Gender</label>
+                                        <select class="form-control">
+                                            <option>Male</option>
+                                            <option>Female</option>
+                                            <option>Choose not to answer</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group">
+                                        <h3>Artist Contact</h3>
+                                        <label for="exampleFormControlInput1">Email Address</label>
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@hotmail.com">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleFormControlInput1">Phone Number</label>
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="630 420 6666">
+                                    </div>
+                                </div>
+                                <div class="col-12"> 
+                                    <div class="row">
+                                        <div class="col-2" style="padding-right: 5px !important;">
+                                            <div class="form-group">
+                                                <button type="create" class="btn btn-success" style="width: 100%;">Create</button>
+                                            </div>
+                                        </div>
+                                        <div class="col-2" style="padding-left: 5px !important; padding-right: 5px !important;">
+                                            <div class="form-group">
+                                                <button type="cancel" class="btn btn-danger" style="width: 100%;">Cancel</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
