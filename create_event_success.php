b27cb3f7a520cb0d0265c6e1392a101acd53366a
Learnallthetime <philipboffa@gmail.com>
2018-11-07 00:19:55 -0600

    Prepared more php for Event.

--- create_event_success.php
+++ create_event_success.php
@@ -4,23 +4,21 @@
 
 
         <?php
-                $Name = trim($_POST['street']);
-                $band_Members = trim($_POST['city']);
-                $pay_Rate = trim($_POST['state']);
-                $special_Notes = trim($_POST['zip']);
-                $band_Lead_Number = trim($_POST['str_Date']);
-                $agent_Street = trim($_POST['end_Date']);
-                $agent_City = trim($_POST['status']);
-                $agent_State = trim($_POST['capacity']);
-                $agent_Zip_Code = trim($_POST['ag_ZipCode']);
-                $agent_Email = trim($_POST['ag_Email']);
-                $agent_Phone = trim($_POST['ag_Phone']);
+                $Street = trim($_POST['street']);
+                $City = trim($_POST['city']);
+                $State = trim($_POST['state']);
+                $Zip = trim($_POST['zip']);
+                $Start_Date = trim($_POST['str_Date']);
+                $End_Date = trim($_POST['end_Date']);
+                $Status = trim($_POST['status']);
+                $Capacity = trim($_POST['capacity']);
+
 
                 if($_SERVER['REQUEST_METHOD'] == 'POST')
                 {
-$q = "INSERT INTO Event (street, city, state, zip, status, capacity) VALUES (?, ?, ?, ?, ?, ?)
+$q = "INSERT INTO Event (street, city, state, zip, str_Date, end_Date status, capacity) VALUES (?, ?, ?, ?, ?, ?);";
 $query = $pdo->prepare($q);
-$results = $query->execute(array($band_Name, $band_Members, $pay_Rate, $special_Notes, $band_Lead_Number, $agent_Street, $agent_City, $agent_Stat$
+$results = $query->execute(array($Street, $City, $State, $Zip, $Start_Date, $End_Date, $Status, $Capacity));
 
 
 
