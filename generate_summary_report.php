9f0ade06fdfc733fddd7a2964a83329776ca60e0
Learnallthetime <philipboffa@gmail.com>
2018-11-08 05:19:13 -0600

    Started php in phil rep gen.

--- generate_summary_report.php
+++ generate_summary_report.php
@@ -13,7 +13,7 @@
 
         <section>
             <div class="container">
-                <form method="post" action="phil_gen_report.php">
+                <form method="post" action="phil_gen_rep.php">
                     <div class="row">
                         <div class="col-3">
                             <div class="boxify">
--- phil_gen_rep.php
+++ phil_gen_rep.php
@@ -3,10 +3,23 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-$StartDate = $_POST(['StartDate']);
+//$StartDate = $_POST(['StartDate']);
 
-$qr = "SELECT * FROM Event WHERE str_Date= '".$StartDate."' LIMIT 0 , 1";
-$query->execute(array($StartDate));
+//$qr = "SELECT * FROM Event WHERE str_Date= '".$StartDate."' LIMIT 0 , 1";
+//$query->execute(array($StartDate));
+
+
+
+// Read input from $_POST
+$StartDate = (isset($_POST['StartDate']) ? $_POST['StartDate'] : '');
+
+// Build query.  Properly escape input data.
+$query = 
+  "SELECT * " .
+  "FROM Events " .
+  "WHERE str_Date = '" . mysql_real_escape_string($StartDate) . "' " . 
+  "LIMIT 0,1";
+
 }
 
 ?>
